1c448e3718cb212995dd3ca76a7db0c691f91e6d
Webmaster anonymail <webmaster31anonymail@gmail.com>
2021-04-24 15:57:31 +0200

    null

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const ws=require('ws');
 const handler=require('./mainHandler.js'); 
 const config=require("./config.json");
 const httpServer=http.createServer(handler.http);
-const wsServer=new ws.Server(config.ws,config.host);
+const wsServer=new ws.Server(config.ws);
+var app_port = process.env.app_port || 8080;
+var app_host = process.env.app_host || '127.0.0.1';
 console.log("Démarage");
-httpServer.listen(config.http.port);
+httpServer.listen(app_port);
 wsServer.on("connection",handler.ws)
