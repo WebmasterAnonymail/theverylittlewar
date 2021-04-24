92061138fec6358196abd4a695a6dfdf4fcd269b
Webmaster anonymail <webmaster31anonymail@gmail.com>
2021-04-24 14:35:10 +0200

    Début du git

--- functions/usercheck.js
+++ functions/usercheck.js
@@ -1,10 +1,5 @@
-mysql=require("mysql")
-config=require("../config.json")
-bdd=mysql.createConnection(config.mysql);
-bdd.connect();
 module.exports=function(userid,token){
     now_date=new Date();
-    console.log(mysql.escape(now_date))
+    console.log()
     //a mettre a la fin
-    bdd.end(); 
 }
