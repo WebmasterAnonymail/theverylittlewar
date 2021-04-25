8662885cb3196bf857c03711a1b28d35252b191c
Webmaster anonymail <webmaster31anonymail@gmail.com>
2021-04-25 09:36:24 +0200

    simple revision

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const nano=require('nano');
 const handler=require('./mainHandler.js'); 
 const config=require("./config.json");
 const httpServer=http.createServer(handler.http);
-const wsServer=new ws.Server({server:httpServer,path:"/api"});
+const wsServer=new ws.Server({server:httpServer});
 console.log("Démarage");
 httpServer.listen(config.http.port);
 wsServer.on("connection",handler.ws)
