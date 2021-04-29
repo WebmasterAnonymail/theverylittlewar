19344aa72fe066766636c39c71c8419c68152898
Webmaster anonymail <webmaster31anonymail@gmail.com>
2021-04-29 18:16:08 +0200

    simple revision

--- view/js/main.js
+++ view/js/main.js
@@ -4,8 +4,21 @@ window.onload=function(ev){
         document.getElementById("popup_mask").addEventListener("click",(ev)=>{
             document.getElementById("popup_mask").style.display="none";
             document.getElementById(opened_iframe_id).setAttribute("open","no");
-        })
+        });
     }
+	let api_xhr=new XMLHttpRequest();
+	let at_send=new URLSearchParams();
+	at_send.append("token",localStorage.getItem("token"))
+	api_xhr.open("GET","/api/v1/connect?"+at_send.toString());
+	api_xhr.responseType="json";
+	api_xhr.send(JSON.stringify(at_send));
+	api_xhr.addEventListener("readystatechange",function(ev){
+		if(api_xhr.readyState==api_xhr.DONE){
+			if(!api_xhr.response.connected){
+				document.location.replace("html/accueil.html");
+			}
+		}
+	});
 }
 function open_iframe(iframeid){
     let iframe=document.getElementById("iframe"+iframeid);
