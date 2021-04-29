f0a6067961c5278e536ff58edec95bff3b3b5390
Webmaster anonymail <webmaster31anonymail@gmail.com>
2021-04-29 14:04:12 +0200

    simple revision

--- staticHandler.js
+++ staticHandler.js
@@ -1,24 +1,33 @@
 const fs=require("fs");
+const ext_types={
+	"html":"text/html",
+	"css":"text/css",
+	"js":"application/javascript",
+	"svg":"image/svg+xml",
+	"png":"image/png",
+	"ico":"image/x-icon",
+	"gif":"image/gif"
+};
 module.exports=function(req,res){
     let url=req.url;
-    if(url=="/"){
-        url="/main.html";
-    }
-    try{
-        file=fs.readFileSync("view"+url);
-        res.writeHead(200);
-        res.write(file);
-        res.end();
-    }catch(err){
-        if(err.code=='ENOENT'){
-            res.writeHead(404);
-            res.write("404");
-            res.end();
-        }else{
-            res.writeHead(500);
-            res.write("500");
-            res.end();
-        }
-    }
-    
+	if(url=="/"){
+		url="/main.html";
+	}
+	extension=url.substring(url.lastIndexOf('.')+1);
+	try{
+		file=fs.readFileSync("view"+url);
+		res.writeHead(200,{'Content-Type':ext_types[extension]});
+		res.write(file);
+		res.end();
+	}catch(err){
+		if(err.code=='ENOENT'){
+			res.writeHead(400);
+			res.write("400");
+			res.end();
+		}else{
+			res.writeHead(500);
+			res.write("500");
+			res.end();
+		}
+	}
 }
