10428e085f7effded75cc92351c7300837cf7756
Webmaster anonymail <webmaster31anonymail@gmail.com>
2021-08-27 13:29:46 +0200

    Création de miscdatas.js

---
+++ functions/miscdatas.js
@@ -0,0 +1,56 @@
+module.exports.atomes=[
+	"carbone",
+	"oxygene",
+	"azote",
+	"iode",
+	"brome",
+	"hydrogene",
+	"soufre",
+	"chlore"
+];
+module.exports.medailles=[
+	"def",
+	"atk",
+	"mol",
+	"tps",
+	"prt",
+	"des",
+	"pil",
+	"cmb"
+];
+module.exports.batiment_augmentateurs=[
+	"forteresse",
+	"ionisateur",
+	"lieur",
+	"stabilisateur",
+	"champdeforce",
+	"usinedexplosif",
+	"condenseur",
+	"booster"
+];
+module.exports.seuils_medailes=[1,2,5,10,20,50,100,200,500,1000];
+module.exports.multiplacateur_medailles=[1000,1000,100,50,50,1000,1000,1];
+module.exports.points_medailles=[
+	"defense",
+	"attaque",
+	"molecules_crees",
+	"pertes_temps",
+	"pertes_combat",
+	"destruction",
+	"pillage",
+	"combats"
+];
+module.exports.points_batiments={
+	"generateur":1,
+	"producteur":1,
+	"stockage":1,
+	"forteresse":3,
+	"ionisateur":3,
+	"lieur":3,
+	"stabilisateur":3,
+	"champdeforce":3,
+	"usinedexplosif":3,
+	"condenseur":3,
+	"booster":3,
+	"protecteur":2
+};
