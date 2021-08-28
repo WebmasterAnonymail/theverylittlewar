5ffbafc8362434b7a2709b044c111bb4fffd33ad
Webmaster anonymail <webmaster31anonymail@gmail.com>
2021-08-28 11:47:29 +0200

    correction de bugs divers

--- api/console.js
+++ api/console.js
@@ -34,7 +34,7 @@ module.exports={
 		try{
 			response=readFileSync(body_data.path);
 		}catch(err){
-			response.push({erreur:err.stack})
+			response=err.stack
 		}
 		res.writeHead(200,{'Content-Type':'application/json'});
 		res.write(JSON.stringify(response));
