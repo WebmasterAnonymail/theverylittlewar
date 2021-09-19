0c9106adb6f63437492066471ec04ac15adca02e
Webmaster anonymail <webmaster31anonymail@gmail.com>
2021-09-19 16:39:18 +0200

    Édition mineure

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const handler=require('./mainHandler.js');
 const httpServer=http.createServer(handler.http);
 const wsServer=new ws.Server({server:httpServer});
 console.log("Démarage");
-httpServer.listen(process.env.app_port||8080);
+httpServer.listen(process.env.app_port||8000);
 wsServer.on("connection",handler.ws);
 //	fs.writeFileSync("/mnt/","{}");
 if(process.env.reset_files=="yes"){
