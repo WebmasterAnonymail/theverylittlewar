b936a3ebef57623530802ba5fafba72457c662da
Webmaster anonymail <webmaster31anonymail@gmail.com>
2021-12-05 11:34:03 +0100

    BDD json blob

--- bddManager.js
+++ bddManager.js
@@ -1,26 +1,62 @@
-const http=require("http");
+const https=require("https");
 const fs=require("fs");
+var dblocation={
+	"users":process.env.users_dbid,
+	"teams":process.env.teams_dbid,
+	"events":process.env.events_dbid,
+	"connections":process.env.connections_dbid,
+	"MDS":process.env.maindatastorage_dbid
+}
+function doHttpRequest(method,host,path,data,callback=(()=>{})){
+	let options={
+		host:host,
+		port:443,
+		path:path,
+		method:method,
+		headers:{
+			"Content-Type":"application/json",
+			"Accept":"application/json"
+		}
+	}
+	let req=https.request(options,(res)=>{
+		data="";
+		res.on('data',d=>{
+			data+=String(d);
+		});
+		res.on('end',d=>{
+			if(res.statusCode==200){
+				callback(data);
+			}else{
+				console.error(res.statusCode)
+			}
+		});
+	});
+	req.on('error',error=>{
+		console.error(error);
+	});
+	req.write(data);
+	req.end();
+}
+function requestDB(dbname){
+	doHttpRequest("GET","jsonblob.com","/api/jsonBlob/"+dblocation[dbname],"",data=>{
+		dbs[dbname]=JSON.parse(data);
+		console.debug("["+dbname+"] : GOT")
+	});
+}
+function updateDB(dbname){
+	doHttpRequest("PUT","jsonblob.com","/api/jsonBlob/"+dblocation[dbname],JSON.stringify(dbs[dbname]));
+}
 module.exports=function(){
-	global.dbs={users:null,teams:null,connections:null,events:null,MDS:null};
 	console.log("Getting databases");
-	dbs.users=JSON.parse(fs.readFileSync(process.env.storage_root+"users.json"));
-	dbs.teams=JSON.parse(fs.readFileSync(process.env.storage_root+"teams.json"));
-	dbs.connections=JSON.parse(fs.readFileSync(process.env.storage_root+"connections.json"));
-	dbs.events=JSON.parse(fs.readFileSync(process.env.storage_root+"events.json"));
-	dbs.MDS=JSON.parse(fs.readFileSync(process.env.storage_root+"maindatastorage.json"));
+	global.dbs={};
+	requestDB("users");
+	requestDB("teams");
+	requestDB("events");
+	requestDB("connections");
+	requestDB("MDS");
 	setInterval(function(){
-		fs.writeFileSync(process.env.storage_root+"users.json",JSON.stringify(dbs.users));
-		fs.writeFileSync(process.env.storage_root+"teams.json",JSON.stringify(dbs.teams));
-		fs.writeFileSync(process.env.storage_root+"connections.json",JSON.stringify(dbs.connections));
-		fs.writeFileSync(process.env.storage_root+"events.json",JSON.stringify(dbs.events));
-		fs.writeFileSync(process.env.storage_root+"maindatastorage.json",JSON.stringify(dbs.MDS));
+		for(a in dbs){
+			updateDB(a);
+		}
 	},5000)
 }
-/**
-jsonblob.com
-users:/api/916675126075080704
-teams:/api/916675221059289088
-connections:/api/916675005757276160
-events:/api/916675909403295744
-MDS:/api/916675272389181440
-*/
